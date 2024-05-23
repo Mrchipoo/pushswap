6cb1d99ea77e7baae9920982db42f7bf85231d77
El Amine Choubby <echoubby@c3r8p6.1337.ma>
2024-05-23 11:03:55 +0100

    halo

--- sort/sort.c
+++ sort/sort.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_3.c                                           :+:      :+:    :+:   */
+/*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: echoubby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 10:38:18 by echoubby          #+#    #+#             */
-/*   Updated: 2024/05/23 10:38:20 by echoubby         ###   ########.fr       */
+/*   Updated: 2024/05/23 11:03:35 by echoubby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/test.h"
 
-void ft_small_sort(walo **head)
+void	ft_small_sort(walo **head)
 {
-    walo *biggest_node;
+	walo	*biggest_node;
 
-    biggest_node = ft_biggest_value(*head);
-    if ((*head) == biggest_node)
-        ft_rotate_a(head);
-    else if ((*head)->next == biggest_node)
-        ft_rra(head);
-    if ((*head)->data > ((*head)->next)->data)
-        ft_swap_a(head);
+	biggest_node = ft_biggest_value(*head);
+	if ((*head) == biggest_node)
+		ft_rotate_a(head);
+	else if ((*head)->next == biggest_node)
+		ft_rra(head);
+	if ((*head)->data > ((*head)->next)->data)
+		ft_swap_a(head);
 }
--- utiliti/help.c
+++ utiliti/help.c
@@ -6,31 +6,30 @@
 /*   By: echoubby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 10:14:38 by echoubby          #+#    #+#             */
-/*   Updated: 2024/05/23 10:14:42 by echoubby         ###   ########.fr       */
+/*   Updated: 2024/05/23 11:02:34 by echoubby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/test.h"
 
-walo *ft_biggest_value(walo *head)
+walo	*ft_biggest_value(walo *head)
 {
-    walo *current;
-    walo *node;
-    int max;
+	walo	*current;
+	walo	*node;
+	int	max;
 
-    current = head->next;
-    node = head;
-    max = head->data;
-    while (current != NULL)
-    {
-        if (max > current->data)
-            current = current->next;
-        else
-        {
-            max = current->data;
-            node = current;
-            current = current->next;
-        }
-    }
-    return (node);
+	current = head->next;
+	node = head;
+	max = head->data;
+	while (current != NULL)
+	{
+		if (max > current->data)
+			current = current->next;
+		else
+		{
+			max = current->data;
+			node = current;
+			current = current->next;
+		}
+	}
+	return (node);
 }
-
